63ee47d74d0fd708641ca66c7bb0e96b1cebc4f2
VasylMolnar <108887823+VasylMolnar@users.noreply.github.com>
2023-06-13 17:35:53 +0000

    Deploying to gh-pages from @ VasylMolnar/Inkrypto@3e343b5fc39f08548bdffeff0bb68a474b6e86c9 🚀

--- src.0bf892fa.js
+++ src.0bf892fa.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/styles.css"),console.log("Майстерня: деплой білда від Parcel на GitHub Pages");
 },{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-deploy-tutorial/src.242d2d4f.js.map
+//# sourceMappingURL=/parcel-deploy-tutorial/src.0bf892fa.js.map
