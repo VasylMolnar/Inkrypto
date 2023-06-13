e569962841b98209a9c2deb384ff87fcf4c10303
VasylMolnar <vasylmolnar281@gmail.com>
2023-06-13 15:44:56 +0300

    Updates

---
+++ index.5d39146f.js
@@ -0,0 +1,2 @@
+console.log("Майстерня: деплой білда від Parcel на GitHub Pages");
+//# sourceMappingURL=index.5d39146f.js.map
--- index.81919b80.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".imageFile");e.addEventListener("change",()=>{console.log(e)}),console.log("HI");
-//# sourceMappingURL=index.81919b80.js.map
