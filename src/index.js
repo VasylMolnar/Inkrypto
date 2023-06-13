05b5ea55544cd3d0d0c2153ad8cdff5f5cd2fe1b
VasylMolnar <vasylmolnar281@gmail.com>
2023-06-13 22:09:56 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './css/styles.css';
 
-const imageFile = document.querySelector('.imageFile');
+const fileInput = document.querySelector('.fileInput');
+const divElement = document.querySelector('.imageFile');
+const btnReset = document.querySelector('.btn-warning');
 
-imageFile.addEventListener('change', () => {
-  console.log(imageFile);
+fileInput.addEventListener('change', () => {
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    let imgElement = `<img src=${e.target.result} class="img" >`;
+    let btnReset = `<btn class="btn btn-warning" style.margin = "50px">Reset</btn>`;
+
+    divElement.innerHTML = '';
+    divElement.innerHTML += imgElement;
+    divElement.insertAdjacentHTML('afterend', btnReset);
+  };
+
+  reader.readAsDataURL(file);
 });
-
-console.log('HI');
