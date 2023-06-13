a0f226d09135bd836be9e38ee1dd875693e05f8b
VasylMolnar <vasylmolnar281@gmail.com>
2023-06-13 15:58:15 +0300

    new

---
+++ src copy2/index.js
@@ -0,0 +1,9 @@
+import './css/styles.css';
+
+const imageFile = document.querySelector('.imageFile');
+
+imageFile.addEventListener('change', () => {
+  console.log(imageFile);
+});
+
+console.log('HI');
